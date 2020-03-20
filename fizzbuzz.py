ff522cea75fde14bbdb6ab401c869ad5f1b41d98
Kevin(Yuxuan) Yang <kevinyang970@hotmail.com>
2020-03-20 12:51:50 +1100

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+
+def fizzbuzz(input_num: int): 
+    if (input_num % 3 == 0 and input_num % 5 == 0):
+        return "FizzBuzz"
+    elif (input_num % 3 == 0):
+        return "Fizz"
+    elif (input_num % 5 == 0):
+        return "Buzz"
+    else:
+        return str(input_num)
+    
+if __name__ == '__main__':
+    print(map(fizzbuzz, range(1,101)))
